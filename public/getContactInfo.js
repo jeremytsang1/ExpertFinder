579026e4ee3f7f76e7cadb0dbf21c410cddae4a6
chavemar <chavemar@oregonstate.edu>
2020-11-07 16:31:24 -0800

    added button to get contcat info

---
+++ public/getContactInfo.js
@@ -0,0 +1,6 @@
+function getContactInfo(id)
+{
+    console.log("Caling the function getConractInfo(" + id + ")");
+    window.location = '/searchResults/' + id + '/contactInfo/'
+
+};
--- searchResults.js
+++ searchResults.js
@@ -4,18 +4,6 @@ module.exports = function(){
     var router = express.Router();
     const fs = require('fs'); 
 
-    // function getClasses(res, mysql, context, complete){
-    //     var sql =  "SELECT c.ClassID, c.ClassDepartment, c.ClassNumber, c.Section, c.ClassDate, c.ClassTime, c.TeacherID FROM Classes c";
-    //     mysql.pool.query(sql, function(error, results, fields){
-    //         if(error){
-    //             res.write(JSON.stringify(error));
-    //             res.end();
-    //         }
-    //         context.classes = results;
-    //         complete();
-    //     });
-    // }
-
     // souce: https://www.geeksforgeeks.org/how-to-read-and-write-json-file-using-node-js/
     function getAllUsers(res, context, complete){
         // Read db.json file 
@@ -28,10 +16,11 @@ module.exports = function(){
             // Converting to JSON 
             const dbUsers = JSON.parse(data); 
             //console.log(users); // Print users  
-            //console.log(users.users[0].name)
             context.users = dbUsers.users;
+            console.log("In function getAllUsers. Printing context")
+            console.log(context)
+            complete();
         }); 
-        complete();
         // const db_users = require('./db.json');
         // console.log(db_users);
     }
@@ -52,62 +41,39 @@ module.exports = function(){
         });
 
         // search all users to match ID. if a match then save the contact information
-        
 
-        complete();
-
-
-        
+        complete();    
     }
 
-    // function getClubs(res, mysql, context, complete){
-    //     var sql =  "SELECT c.ClubID, c.ClubName, c.Category, c.ClubDate, c.ClubTime, c.Sponsor, c.Funding, c.Sponsor FROM Clubs c";
-    //     mysql.pool.query(sql, function(error, results, fields){
-    //         if(error){
-    //             res.write(JSON.stringify(error));
-    //             res.end();
-    //         }
-    //         context.clubs = results;
-    //         complete();
-    //     });
-    // }
-
-    // function getClass(res, mysql, context, classId, complete){
-    //     var sql = "SELECT c.ClassID, c.TeacherID, c.ClassDepartment, c.ClassNumber, c.Section, c.ClassDate, c.ClassTime FROM Classes c \
-    //                WHERE c.ClassID = ?";
-    //     var inserts = [classId];
-    //     mysql.pool.query(sql, inserts, function(error, results, fields){
-    //         if(error){
-    //             res.write(JSON.stringify(error));
-    //             res.end();
-    //         }
-    //         context.class = results[0];
-    //         complete();
-    //     });
-    // }
-
-    // function getClub(res, mysql, context, clubId, complete){
-    //     var sql = "SELECT c.ClubID, c.ClubName, c.Category, c.ClubDate, c.ClubTime, c.Sponsor, c.Funding FROM Clubs c \
-    //                WHERE c.ClubID = ?";
-    //     var inserts = [clubId];
-    //     mysql.pool.query(sql, inserts, function(error, results, fields){
-    //         if(error){
-    //             res.write(JSON.stringify(error));
-    //             res.end();
-    //         }
-    //         context.club = results[0];
-    //         complete();
-    //     });
-    // }
 
     router.get('/', function(req, res){
         var callbackCount = 0;
         var context = {};
+        context.jsscripts = ["getContactInfo.js"];
+        console.log(context)
         getAllUsers(res, context, complete);
         // context.jsscripts = ["deleteClass.js","deleteClub.js","updateClub.js", "updateClass.js"];
         // var mysql = req.app.get('mysql');
         // getClasses(res, mysql, context, complete);
         // getClubs(res, mysql, context, complete);
+
+        // force syncronous flow (make sre getUsers() function finishes executing before proceedding to next line of of code)
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 1){
+                console.log("In get method");
+                console.log(context);
+                res.render('searchResults', context)
+            }
+        }
+    });
+
+    // Display contact info for specific user ID
+    router.get('/:id/contactInfo', function(req, res){
+        console.log("inside router.get(searchResults/:id/contactInfo)")
+        var callbackCount = 0;
+        var context = {};
+        getContactInfoById(res, context, id, complete)
 
         // force syncronous flow (make sre getUsers() function finishes executing before proceedding to next line of of code)
         function complete(){
