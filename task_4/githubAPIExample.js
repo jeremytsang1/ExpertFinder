c37a9d6df4de97a93b69e3e8eae1c8542fc78fdf
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-12 17:15:32 -0800

    Add comments

--- task_4/githubAPIExample.js
+++ task_4/githubAPIExample.js
@@ -38,23 +38,28 @@ function makeGitHubURL() {
 
 function registerGitHubCallback(req) {
   req.addEventListener("load", () => {
+    // DOM element whose .textContent will be modified with depending on status of
+    // the response.
     let outputElt = document.querySelector("#output-repos");
 
     if (req.status >= 200 && req.status < 400) { // Search successful.
       let res = JSON.parse(req.responseText);
       outputElt.textContent = listGitHubRepos(res)
     } else { // Search failed.
-
       outputElt.textContent = "No public repos found for that username!";
     }
   });
 }
 
 function listGitHubRepos(res) {
-  let output = [];
+  let output = [];  // Each element will represent output for a specific repo.
 
+  // Get the repo name and URL from the response object.
   res.forEach(repo => {
     output.push(`${repo['name']}: ${repo['html_url']}`)
   });
+
+  // Using '\n' for newline assumes output element is styled with 'white-space:
+  // pre-line'.
   return output.join('\n\n');
 }
