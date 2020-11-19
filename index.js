65366d3ac3df312be66ff865c186e1b2de2bd76d
chavemar <chavemar@oregonstate.edu>
2020-11-19 13:36:04 -0800

    made profile links clickable, created a handlebars helper function, updated port to 3500 in script

--- index.js
+++ index.js
@@ -18,8 +18,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Handlebars middleware
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
-app.engine('handlebars', handlebars.engine);
+var handlebars = require('express-handlebars');
+
+//Custome handlebars helpers
+//See https://www.npmjs.com/package/express-handlebars
+var hbs = handlebars.create({
+  // Specify helpers which are only registered on this instance.
+  helpers: {
+      // for testing
+      // foo: function () { return 'FOO!'; },
+      // bar: function () { return 'BAR!'; },
+      ifEquals: function(arg1, arg2, options) {
+        console.log("Testing ", arg1, " == ", arg2, ": ", arg1 == arg2)
+        return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+    }
+  }
+});
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', 3500);
