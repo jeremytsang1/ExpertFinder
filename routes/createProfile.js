e027880f944b6b2622c18b7d304001881fb381c1
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-19 21:40:16 -0800

    Move callback helpers to separate module

--- routes/createProfile.js
+++ routes/createProfile.js
@@ -2,28 +2,6 @@ module.exports = function() {
   var express = require('express');
   var router = express.Router();
 
-  function runCallbacksAndRender(res, template, context, callbacks) {
-    if (callbacks.length == 0) res.render(template, context); // render immediately
-    else renderAfterCallbacksComplete(res, template, context, callbacks);
-  }
-
-  function renderAfterCallbacksComplete(res, template, context, callbacks) {
-    let callbacksCompletedCount = 0;
-
-    // run all the callbacks and render when they've all completed
-    callbacks.forEach(callback => callback(complete))
-
-    // ------------------------------------------------------------------------
-    // Helper functions for renderAfterCallbacksComplete()
-
-    // relies on closure for callbacksCompletedCount
-    function complete() {
-      callbacksCompletedCount++;
-      // Check if callback is the last callback to complete
-      if (callbacksCompletedCount == callbacks.length) res.render(template, context);
-    }
-  }
-
   router.get('/', function(req, res) {
     var context = {
       cssstyles: [
