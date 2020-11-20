63e7e83d03f9603fa7c6984c46ad4e3fd382d90b
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-20 00:14:28 -0800

    Add extra styling from official tagify repo
    Take example from "Text Input" example in
    https://github.com/yairEO/tagify/blob/master/index.html#L1456

--- public/scripts/profileCreationTags.js
+++ public/scripts/profileCreationTags.js
@@ -57,9 +57,17 @@ function profileCreationTagsMain() {
   }
 
   function createTagify(cat, suggestions) {
+    // Taken from Official tagify repo:
+    // https://github.com/yairEO/tagify/blob/bd4cb069b1cacb6b8bdd34457de5f86752015a9c/index.html#L1456
     return new Tagify(INPUTS.get(cat), {
       whitelist: suggestions[cat].sort(),
-      maxTags: 5
+      maxTags: 5,
+      dropdown: {
+        maxItems: 20,           // <- mixumum allowed rendered suggestions
+        classname: "tags-look", // <- custom classname for this dropdown, so it could be targeted
+        enabled: 0,             // <- show suggestions on focus
+        closeOnSelect: false    // <- do not hide the suggestions dropdown once an item has been selected
+      }
     });
   }
 }
