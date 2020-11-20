de82c0dd8077b7e059c1ded23610d1eba375be5a
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-19 23:09:20 -0800

    Retructure into functions

--- public/scripts/profileCreationTags.js
+++ public/scripts/profileCreationTags.js
@@ -1,14 +1,34 @@
-const INPUT_ID_PREFIX = "profile-creation-";
-const INDUSTRY = "Industry";
-const SKILLS = "TechSkills";
-const COURSEWORK = "Coursework";
-const CATEGORIES = [INDUSTRY, SKILLS, COURSEWORK];
+function profileCreationTagsMain() {
+  const INPUT_ID_PREFIX = "profile-creation-";
+  const INDUSTRY = "Industry";
+  const SKILLS = "TechSkills";
+  const COURSEWORK = "Coursework";
+  const CATEGORIES = [INDUSTRY, SKILLS, COURSEWORK];
 
-let inputs = new Map();
-let tagifies = new Map();
+  let inputs = gatherInputs();
+  let tagifies = initializeTagifies(inputs);
 
-CATEGORIES.forEach(cat => {
-  input = document.querySelector(`#${INPUT_ID_PREFIX}${cat.toLowerCase()}`);
-  inputs.set(cat, input);
-  tagifies.set(cat, new Tagify(input));
-});
+  // --------------------------------------------------------------------------
+  // Helper functions (keep nested as not to pollute namespace).
+
+  function gatherInputs() {
+    let inputs = new Map();
+
+    CATEGORIES.forEach(cat => {
+      input = document.querySelector(`#${INPUT_ID_PREFIX}${cat.toLowerCase()}`);
+      inputs.set(cat, input);
+    });
+    return inputs;
+  }
+
+  function initializeTagifies(inputs) {
+    let tagifies = new Map();
+
+    inputs.forEach((val, key, map) => {
+      tagifies.set(key, new Tagify(val));
+    });
+    return tagifies;
+  }
+}
+
+profileCreationTagsMain();
