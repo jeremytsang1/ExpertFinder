3d5cd104a712f88cc58b57d87b160e89e3f3a970
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-21 02:03:17 -0800

    Remove limit for permissible tags

--- public/scripts/profileCreationTags.js
+++ public/scripts/profileCreationTags.js
@@ -61,7 +61,6 @@ function profileCreationTagsMain() {
     // https://github.com/yairEO/tagify/blob/bd4cb069b1cacb6b8bdd34457de5f86752015a9c/index.html#L1456
     return new Tagify(INPUTS.get(cat), {
       whitelist: suggestions[cat].sort(),
-      maxTags: 5,
       dropdown: {
         maxItems: 20,           // <- mixumum allowed rendered suggestions
         classname: "tags-look", // <- custom classname for this dropdown, so it could be targeted
