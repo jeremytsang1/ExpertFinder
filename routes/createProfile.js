7ccb5a20545ac61418f45d0b913274378eb29106
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-21 06:36:33 -0800

    Code Smell: Shotgun on hardcoded HTML code
    Squashed commit of the following:
    
    commit 3b548e7410dd86584cb760cea81868735407d4ba
    Author: Jeremy Tsang <jeremytsang1@gmail.com>
    Date:   Sat Nov 21 06:26:40 2020 -0800
    
        Jeremy Code Smell 1: Shotgun
    
        Finish refactor the hardcoded HTML using DOM manipulation instead.
    
        Jeremy Code Smell 1 commit-9 (final): Shotgun
    
    commit 7bff21500e783cf9e39754c7568f726a9a046ce6
    Author: Jeremy Tsang <jeremytsang1@gmail.com>
    Date:   Sat Nov 21 06:19:18 2020 -0800
    
        Finish programmatically placing standard fields
    
        Jeremy Code Smell 1 commit-8: shotgun
    
    commit c6343496299d21e0793b5a79dd3692c6b2e8bbbd
    Author: Jeremy Tsang <jeremytsang1@gmail.com>
    Date:   Sat Nov 21 05:07:10 2020 -0800
    
        Condense two name fields down to 1
    
        Jeremy Code Smell 1 commit-7: shotgun
    
    commit d3b9f06dd5a5b7cffc5f92dd9ae67050e34b2a92
    Author: Jeremy Tsang <jeremytsang1@gmail.com>
    Date:   Sat Nov 21 04:44:07 2020 -0800
    
        Finish programmatically placing standard fields
    
        Jeremy Code Smell 1 commit-6: shotgun
    
    commit b4d56527d2d56b07e4da2f7dcae2b26d99f611a1
    Author: Jeremy Tsang <jeremytsang1@gmail.com>
    Date:   Sat Nov 21 04:06:57 2020 -0800
    
        Remove hardcoded fields in template
    
        Also put field info in profileCreationStructureMain().
    
        Jeremy Code Smell 1 commit-5: shotgun
    
    commit 92a0816c37be01dfbd42d7fa3e0a852b919802cc
    Author: Jeremy Tsang <jeremytsang1@gmail.com>
    Date:   Sat Nov 21 03:57:00 2020 -0800
    
        Make text for <label>
    
        Jeremy Code Smell 1 commit-4: shotgun
    
    commit 082d5f9d4c6b3ef1ad9ec1058ceedcaca989ed42
    Author: Jeremy Tsang <jeremytsang1@gmail.com>
    Date:   Sat Nov 21 03:54:22 2020 -0800
    
        Implement DatabaseField.createInput()
    
        Jeremy Code Smell 1 commit-3: shotgun
    
    commit a289a31783b7fc8cb6b397ab8f76c71fe075bf55
    Author: Jeremy Tsang <jeremytsang1@gmail.com>
    Date:   Sat Nov 21 03:32:14 2020 -0800
    
        Make method to create <label>
    
        Jeremy Code Smell 1 commit-2: shotgun
    
    commit d3f77a18cf3f4d21e35f09cbd547095ada383dea
    Author: Jeremy Tsang <jeremytsang1@gmail.com>
    Date:   Sat Nov 21 03:15:26 2020 -0800
    
        Start class DatabaseField
    
        Jeremy Code Smell 1 commit-1: shotgun
    
        Create to programmatically generate form groups on profile creation form
        to avoid having to change so much by hand.

--- routes/createProfile.js
+++ routes/createProfile.js
@@ -8,6 +8,8 @@ module.exports = function() {
         "public/css/tagify.css"
       ],
       jsscripts: [
+        "profileCreationDatabaseField.js",
+        "profileCreationStructure.js",
         "tagify.min.js",
         "profileCreationTags.js",
         "profileCreationForm.js"
