1fcab8dc5b3c5e6391d0eee6f2428098ed36fce4
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-21 06:46:52 -0800

    Refactor using object instead of using constants
    Jeremy Code Smell 2 commit-1: Primitive Obsession

--- public/scripts/profileCreationTags.js
+++ public/scripts/profileCreationTags.js
@@ -1,27 +1,50 @@
+class TagifyCategory {
+  constructor(htmlName, dbName) {
+    this.htmlName = htmlName;
+    this.dbName = dbName;
+  }
+
+  getInput() {
+    return document.querySelector(`#${TagifyCategory.INPUT_ID_PREFIX}${this.htmlName}`);
+  }
+
+  getHtmlName() {
+    return this.htmlName;
+  }
+
+  getDbName() {
+    return this.dbName;
+  }
+
+  static get INPUT_ID_PREFIX () {
+    return "profile-creation-";
+  }
+
+  createTagify(suggestionAry) {
+    // Taken from Official tagify repo:
+    // https://github.com/yairEO/tagify/blob/bd4cb069b1cacb6b8bdd34457de5f86752015a9c/index.html#L1456
+    return new Tagify(this.getInput(), {
+      whitelist: suggestionAry.sort(),
+      dropdown: {
+        maxItems: 20,           // <- mixumum allowed rendered suggestions
+        classname: "tags-look", // <- custom classname for this dropdown, so it could be targeted
+        enabled: 0,             // <- show suggestions on focus
+        closeOnSelect: false    // <- do not hide the suggestions dropdown once an item has been selected
+      }
+    });
+  }
+}
+
 function profileCreationTagsMain() {
-  const INPUT_ID_PREFIX = "profile-creation-";
-  const INDUSTRY = "Industry";
-  const SKILLS = "TechSkills";
-  const COURSEWORK = "Coursework";
-  const CATEGORIES = [INDUSTRY, SKILLS, COURSEWORK];
   const URL = "/suggestions";
+  const CATEGORIES = [
+    new TagifyCategory("industry", "Industry"),
+    new TagifyCategory("tech-skills", "TechSkills"),
+    new TagifyCategory("coursework", "Coursework"),
+  ];
   const CATEGORIES_KEY = "Categories";
-  const INPUTS = gatherInputs();
 
   getSuggestionsFromServer();
-
-  // --------------------------------------------------------------------------
-  // Helper functions (keep nested to keep namespace clean)
-
-  function gatherInputs() {
-    const inputs = new Map();
-
-    CATEGORIES.forEach(cat => {
-      input = document.querySelector(`#${INPUT_ID_PREFIX}${cat.toLowerCase()}`);
-      inputs.set(cat, input);
-    });
-    return inputs;
-  }
 
   function getSuggestionsFromServer() {
     const req = new XMLHttpRequest();
@@ -33,7 +56,6 @@ function profileCreationTagsMain() {
     req.send(null);
   }
 
-
   function registerSuggestionsCallback(req) {
     req.addEventListener("load", () => {
       if (req.status >= 200 && req.status < 400) handleSuccessfulRequest(req);
@@ -44,30 +66,14 @@ function profileCreationTagsMain() {
   function handleSuccessfulRequest(req) {
     const res = JSON.parse(req.responseText);
     const suggestions = res[CATEGORIES_KEY];
-    const tagifies = new Map();
 
     CATEGORIES.forEach(cat => {  // Fill the map.
-      tagify = createTagify(cat, suggestions);
-      tagifies.set(cat, tagify);
+      cat.createTagify(suggestions[cat.dbName])
     });
   }
 
   function handleFailedRequest(req) {
     console.log("Could not get suggestions!");
-  }
-
-  function createTagify(cat, suggestions) {
-    // Taken from Official tagify repo:
-    // https://github.com/yairEO/tagify/blob/bd4cb069b1cacb6b8bdd34457de5f86752015a9c/index.html#L1456
-    return new Tagify(INPUTS.get(cat), {
-      whitelist: suggestions[cat].sort(),
-      dropdown: {
-        maxItems: 20,           // <- mixumum allowed rendered suggestions
-        classname: "tags-look", // <- custom classname for this dropdown, so it could be targeted
-        enabled: 0,             // <- show suggestions on focus
-        closeOnSelect: false    // <- do not hide the suggestions dropdown once an item has been selected
-      }
-    });
   }
 }
 
