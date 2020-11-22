92242456664e8dd2e180e26119ce221bf32dab81
K-Concept <Kagaminelove02@gmail.com>
2020-11-21 21:35:26 -0800

    Update SuggestedEditsForm.js

--- public/scripts/SuggestedEditsForm.js
+++ public/scripts/SuggestedEditsForm.js
@@ -3,5 +3,9 @@ SuggestEditsButton.addEventListener("click", updateForm);
 
 function updateForm(event)
 {
-
+  /* This wouldn't work cause the value isn't an input?
+  {{this.TechSkills}}: row.querySelector("input[name=Techskills]").value,
+  {{this.Coursework}}: row.querySelector("input[name=Coursework]").value,
+  {{Industry.Industry}}: row.querySelector("input[name=Industry]").value,
+  /*
 }
