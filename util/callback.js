ab271b347fab77c83df478b98332868c40e4f99a
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-24 15:56:47 -0800

    Put all callback related helpers in 1 file
    Make the callbackUtil file into a static method in class Callback.

--- util/callback.js
+++ util/callback.js
@@ -3,6 +3,23 @@ class Callback {
     this.fcn = fcn; // has parameters (complete, responseAction)
     this.actionIfLastCallback = responseAction; // action
   }
+
+  static runCallbacks(callbacks) {
+    let callbacksCompletedCount = 0;
+
+    // run all the callbacks and send when they've all completed
+    callbacks.forEach(callback => callback.fcn(complete, callback.actionIfLastCallback))
+
+    // ------------------------------------------------------------------------
+    // Helper functions for sendAfterCallbacksComplete()
+
+    // relies on closure for callbacksCompletedCount
+    function complete(actionIfLastCallback) {
+      callbacksCompletedCount++;
+      // Check if callback is the last callback to complete
+      if (callbacksCompletedCount == callbacks.length) actionIfLastCallback();
+    }
+  }
 }
 
 module.exports = {
