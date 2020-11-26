4ac28f365ac4784f4b0bedfebd5777337fd0967b
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-26 01:47:10 -0800

    Remove unused require statement

--- routes/suggestions.js
+++ routes/suggestions.js
@@ -1,7 +1,6 @@
 const {request} = require('express');
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
 
 // ----------------------------------------------------------------------------
 // Constants
