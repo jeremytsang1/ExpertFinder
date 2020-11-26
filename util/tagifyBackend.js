6d61d75a76abba83c6e2a80e72a07478b4b171f4
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-25 23:30:39 -0800

    Make return code shorter for getTagsAsArray()
    Just use ternary operator, don't bother with printing.

--- util/tagifyBackend.js
+++ util/tagifyBackend.js
@@ -1,12 +1,7 @@
 class TagifyBackend {
   static getTagsAsArray(tagifyInputValues) {
     let formAry = (tagifyInputValues !== "") ? JSON.parse(tagifyInputValues) : null;
-    if (Array.isArray(formAry)) return formAry.map(elt => elt['value']);
-    else {
-      console.log("WARNING: In TagifyBackend.getTags(), `tagifyInputValues` was ",
-                  "not an array after parsing.");
-      return [];
-    }
+    return (Array.isArray(formAry)) ? formAry.map(elt => elt['value']) : [];
   }
 }
 
