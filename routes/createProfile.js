6a952e10bbe2c141b7e33f86916eb0a8fbd101ab
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-27 01:44:31 -0800

    Make route /createProfile to use the API
    Remove the asynchronous file IO functions from createProfile.js and use
    the new API function from db_interface.js instead.
    
    Squashed commit of the following:
    
    commit fd55f685c1232591e6a18c1baf5c8a69eb156654
    Author: Jeremy Tsang <jeremytsang1@gmail.com>
    Date:   Fri Nov 27 01:22:29 2020 -0800
    
        Replace 2 async functions and use API instead
    
        Remove readFile() and writeDatabase() in favor of dbInterface and its
        methods.
    
        Keep the asynchronous funcions for saving the profile picture to the
        server and (not yet implemented) sending the email to the user's email
        address.
    
        Move generation of profile picture paths to its own helper function.
    
    commit 0b40afbab98e6e87a0b8f9a95470afa12ed5f4d2
    Author: Jeremy Tsang <jeremytsang1@gmail.com>
    Date:   Fri Nov 27 01:20:31 2020 -0800
    
        Make function to get number of experts
    
        Also change attribute specification from strings to dot notation for
        consistency with methods already present in the file.
    
    commit 700f5a6ebf5543b65c12cc6d5b081381da281db4
    Author: Jeremy Tsang <jeremytsang1@gmail.com>
    Date:   Fri Nov 27 01:12:19 2020 -0800
    
        Ignore temporary profile pictures directory
    
    commit fb6e5548c149a1911e02027099bcb537c9fa5fe0
    Author: Jeremy Tsang <jeremytsang1@gmail.com>
    Date:   Fri Nov 27 00:15:20 2020 -0800
    
        Add missing semicolon
    
    commit 601580cb6fb28b1f906c225b805eda0bbca000b0
    Author: Jeremy Tsang <jeremytsang1@gmail.com>
    Date:   Fri Nov 27 00:10:31 2020 -0800
    
        Make createExpert() for API

--- routes/createProfile.js
+++ routes/createProfile.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const DATABASE_FILENAME = 'database/db.json';
+const dbInterface = require('../database/db_interface');
 const IMG_DIR = 'public/images';
 const IMG_DIR_TMP = `${IMG_DIR}/tmp`
 const fs = require('fs');
@@ -42,61 +42,52 @@ router.get('/success', (req, res) => {
   const context = {"email": req.query.email};
 
   res.render('createProfileSuccess', context)
-})
+});
 
 router.post('/', upload.single(HTML_NAME_ATTR_OF_IMG_INPUT), function(req, res) {
-  let callbacks = [new Callback(readDatabase, runCallbacksAfterRead)];
-  let db = null;
-  const USER_FORM = JSON.parse([JSON.stringify(req.body)]);
-  const IMG_FILE_TMP_PATH = (req.file) ? req.file.path : null;
-  let imgFileTargetPath = null;
+  const [imgFileTmpPath, imgFileTargetPath] = createProfilePicturePath();
+  const userForm = JSON.parse([JSON.stringify(req.body)]);
+  createNewExpertFromUserForm();
 
+  let callbacks = [new Callback(saveImage, redirectToSuccessPage),
+                   new Callback(sendEmail, redirectToSuccessPage)];
   Callback.runCallbacks(callbacks);
 
-  // --------------------------------------------------------------------------
-  // 1st level of callbacks
-
-  // Read the database from the file and save store it in `db`.
-  function readDatabase(complete, actionIfLastCallback) {
-    fs.readFile(DATABASE_FILENAME, (err, data) => {
-      if (err) handleError(err, res);  // failed read of database file
-      else {
-        db = JSON.parse(data);
-        imgFileTargetPath = ((IMG_FILE_TMP_PATH !== null)
-                             ? `${IMG_DIR}/profile-picture-user-id-${db['NextID']}.png`
-                             : `${IMG_DIR}/female-default-profile-img.jpg`);
-        complete(actionIfLastCallback);
-      }
-    });
+  // ----------------------------------------
+  // helpers
+  function createProfilePicturePath() {
+    const tmpPath = (req.file) ? req.file.path : null;
+    const newUserID = dbInterface.getExpertCount() + 1;
+    const permPath = ((tmpPath !== null)
+                      ? `${IMG_DIR}/id-${newUserID}.png`
+                      : `${IMG_DIR}/female-default-profile-img.jpg`);
+    return [tmpPath, permPath];
   }
 
-  function runCallbacksAfterRead() {
-    let callbacks = [
-      new Callback(writeDatabase, redirectToSuccessPage),
-      new Callback(saveImage, redirectToSuccessPage),
-      new Callback(sendEmail, redirectToSuccessPage)
-    ];
-    Callback.runCallbacks(callbacks);
+  function createNewExpertFromUserForm() {
+    console.log("Writing user creation form data to database.");
+    return dbInterface.createExpert(
+      name=userForm["name"],
+      TechSkills=TagifyBackend.getTagsAsArray(userForm['tech-skills']),
+      Coursework=TagifyBackend.getTagsAsArray(userForm['coursework']),
+      Industry=TagifyBackend.getTagsAsArray(userForm['industry']),
+      ContactInfo={
+        "Email":userForm['email'],
+        "Github":userForm['github'],
+        "Linkedin":userForm['linkedin'],
+        "Twitter":userForm['twitter']
+      },
+      ProfilePicture=imgFileTargetPath);
   }
 
   // ----------------------------------------
-  // 2nd level of callbacks
-
-  function writeDatabase(complete, actionIfLastCallback) {
-    console.log("Writing user creation form data to database.");
-    db = createNewUser(db); // uses db from enclosing scope
-
-    fs.writeFile(DATABASE_FILENAME, JSON.stringify(db, null, 4), err => {
-      if (err) handleError(err, res);
-      else complete(actionIfLastCallback);
-    });
-  }
+  // callbacks
 
   function saveImage(complete, actionIfLastCallback) {
     console.log("Saving image from user creation form to server.");
-    if (IMG_FILE_TMP_PATH === null) complete(actionIfLastCallback);
+    if (imgFileTmpPath === null) complete(actionIfLastCallback);
     else {
-      fs.rename(IMG_FILE_TMP_PATH, imgFileTargetPath, err =>  {
+      fs.rename(imgFileTmpPath, imgFileTargetPath, err =>  {
         if (err) handleError()
         else complete(actionIfLastCallback);
       });
@@ -110,34 +101,9 @@ router.post('/', upload.single(HTML_NAME_ATTR_OF_IMG_INPUT), function(req, res) 
   }
 
   function redirectToSuccessPage() {
-    res.redirect(`createProfile/success?email=${req.body.email}`);
+    const emailAddress = userForm['email'];
+    res.redirect(`createProfile/success?email=${emailAddress}`);
     res.end();
-  }
-
-  // -------------------------
-  // 2nd level callback helpers
-  function createNewUser(database) {
-    let newUser = {
-      "Id":database['NextID'],
-      "Name":USER_FORM["name"],
-      "TechSkills": TagifyBackend.getTagsAsArray(USER_FORM['tech-skills']),
-      "Coursework": TagifyBackend.getTagsAsArray(USER_FORM['coursework']),
-      "Industry": TagifyBackend.getTagsAsArray(USER_FORM['industry']),
-      "ContactInfo":{
-        "Email":USER_FORM['email'],
-        "Github":USER_FORM['github'],
-        "Linkedin":USER_FORM['linkedin'],
-        "Twitter":USER_FORM['twitter']
-      },
-      "ProfilePicture": imgFileTargetPath // from readDatabase()
-    };
-
-    // Chose to use NextID instead of length in case of expert deletion. If
-    // we alloweddeletion we may get duplicate IDs and accidentally overwrite
-    // an existing expert.
-    database['Experts'].push(newUser);
-    database['NextID']++;
-    return database;
   }
 });
 
