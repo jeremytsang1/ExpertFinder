601580cb6fb28b1f906c225b805eda0bbca000b0
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-27 00:10:31 -0800

    Make createExpert() for API

--- database/db_interface.js
+++ database/db_interface.js
@@ -13,9 +13,19 @@ function getExperts(search) {
     return result
 }
 
-function createExpert() {
+function createExpert(name, TechSkills, Coursework, Industry, ContactInfo, ProfilePicture) {
     // name, TechSkills, Coursework, Industry, ContactInfo
     var new_id = test_db["Experts"].length + 1
+    const expert = {
+        "Id": new_id,
+        "Name": name,
+        "TechSkills": TechSkills,
+        "Coursework": Coursework,
+        "Industry": Industry,
+        "ContactInfo": ContactInfo,
+        "ProfilePicture": ProfilePicture
+    }
+    test_db["Experts"].push(expert);
     return new_id
 }
 function updateExperts(id, name, TechSkills, Coursework, Industry, ContactInfo) {
