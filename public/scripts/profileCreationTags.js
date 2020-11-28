b5fda2adbfb6fdd6292221f661492f41a83ddaf0
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-27 19:16:02 -0800

    Update Tagify script to automatically grab fields
    Assumes HTML `name` attribute is identical to the key in suggestion JSON
    from the XHR.

--- public/scripts/profileCreationTags.js
+++ public/scripts/profileCreationTags.js
@@ -1,15 +1,14 @@
-function profileCreationTagsMain() {
+function profileCreationTagsMain(createTagify) {
   const URL = "/suggestions";
-  const CATEGORIES = [
-    new ProfileCreationTagifyCategory("industry", "Industry"),
-    new ProfileCreationTagifyCategory("tech-skills", "TechSkills"),
-    new ProfileCreationTagifyCategory("coursework", "Coursework"),
-  ];
   const CATEGORIES_KEY = "Categories";
+  const inputTags = document.querySelectorAll(".tagify-fields input");
 
-  getSuggestionsFromServer();
+  getSuggestionsFromServerAndCreateTagifyElements();
 
-  function getSuggestionsFromServer() {
+  // ----------------------------------------------------------------------------
+  // Make a GET request to get the auto-complete suggestions for each field.
+
+  function getSuggestionsFromServerAndCreateTagifyElements() {
     const req = new XMLHttpRequest();
 
     req.open("GET", URL, true);
@@ -29,7 +28,7 @@ function profileCreationTagsMain() {
   function handleSuccessfulRequest(req) {
     const res = JSON.parse(req.responseText);
     const suggestions = res[CATEGORIES_KEY];
-    CATEGORIES.forEach(cat => cat.createTagify(suggestions[cat.dbName]));
+    inputTags.forEach(inputTag => createTagify(inputTag, suggestions[inputTag.name]));
   }
 
   function handleFailedRequest(req) {
@@ -41,4 +40,17 @@ function profileCreationTagsMain() {
   }
 }
 
-profileCreationTagsMain();
+function createTagifyForCreateProfile(inputTag, suggestionAry) {
+  // Taken from Official tagify repo:
+  // https://github.com/yairEO/tagify/blob/bd4cb069b1cacb6b8bdd34457de5f86752015a9c/index.html#L1456
+  return new Tagify(inputTag, {
+    whitelist: suggestionAry.sort(),
+    dropdown: {
+      maxItems: 20,           // <- mixumum allowed rendered suggestions
+      classname: "tags-look", // <- custom classname for this dropdown, so it could be targeted
+      enabled: 0,             // <- show suggestions on focus
+      closeOnSelect: false    // <- do not hide the suggestions dropdown once an item has been selected
+    }
+  });
+}
+profileCreationTagsMain(createTagifyForCreateProfile);
