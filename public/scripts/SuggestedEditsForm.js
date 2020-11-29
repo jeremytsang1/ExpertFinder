eb716365c4eccf0b7b452b4f3a3bd594fbefe24b
K-Concept <Kagaminelove02@gmail.com>
2020-11-29 04:13:11 -0800

    Update SuggestedEditsForm.js

--- public/scripts/SuggestedEditsForm.js
+++ public/scripts/SuggestedEditsForm.js
@@ -3,9 +3,10 @@ SuggestEditsButton.addEventListener("click", updateForm);
 
 function updateForm(event)
 {
-  /* This wouldn't work cause the value isn't an input?
   {{this.TechSkills}}: row.querySelector("input[name=Techskills]").value,
   {{this.Coursework}}: row.querySelector("input[name=Coursework]").value,
-  {{Industry.Industry}}: row.querySelector("input[name=Industry]").value,
-  /*
+  {{this.Industry}}: row.querySelector("input[name=Industry]").value,
+  
 }
+  
+let tagify = new Tagify(document.querySelector("#foobar"))
